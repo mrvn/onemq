62b7b4b7529e3e14175f0280b5b406746ce4077a
Goswin von Brederlow <goswin-v-b@web.de>
2015-05-09 11:00:13 +0200

    Add common Msg module

---
+++ common/ocaml/Msg.ml
@@ -0,0 +1,29 @@
+exception ParseError of string
+
+module Hello = struct
+  type t = string
+
+  let net_id = 1
+    
+  let make s = s
+    
+  let length t = String.length t + 1
+end
+
+module Quit = struct
+  type t = unit
+
+  let net_id = 2
+
+  let make () = ()
+    
+  let length _ = 0
+end
+
+module Union = struct
+  type kernel_msg =
+  | Hello of Hello.t
+
+  type peer_msg =
+  | Quit of Quit.t
+end
